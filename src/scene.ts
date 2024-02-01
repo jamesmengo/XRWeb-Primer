1eb4886108ebf01fc3986aa3a1cf0bd0b4d034de
James Meng <james.meng@shopify.com>
2024-02-01 14:39:19 -0800

    Add lighting

--- src/scene.ts
+++ src/scene.ts
@@ -1,71 +1,67 @@
-import { createPlaneMarker } from "./objects/PlaneMarker";
-import { GLTF, GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { handleXRHitTest } from "./utils/hitTest";
+import { createPlaneMarker } from './objects/PlaneMarker';
+import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { handleXRHitTest } from './utils/hitTest';
 
 import {
-  AmbientLight,
-  AxesHelper,
-  DodecahedronGeometry,
-  Mesh,
-  MeshBasicMaterial,
-  Object3D,
-  PerspectiveCamera,
-  Scene,
-  WebGLRenderer,
-  XRFrame,
-} from "three";
+    AmbientLight,
+    Object3D,
+    PerspectiveCamera,
+    Scene,
+    WebGLRenderer,
+    XRFrame,
+} from 'three';
 
 export function createScene(renderer: WebGLRenderer) {
-  let deskModel: Object3D;
+    let deskModel: Object3D;
 
-  const gltfLoader = new GLTFLoader();
-  gltfLoader.load('../assets/models/scene.gltf', (gltf: GLTF) => {
-    deskModel = gltf.scene.children[0];
-  })
+    const scene = new Scene();
+    const camera = new PerspectiveCamera();
 
+    // Load object model
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.load('../assets/models/desk.glb', (gltf: GLTF) => {
+        deskModel = gltf.scene.children[0];
+    });
 
-  const scene = new Scene();
-  const controller = renderer.xr.getController(0);
-  controller.addEventListener("select", () => {
-    if (planeMarker.visible) {
-      const model = deskModel.clone();
-      model.position.setFromMatrixPosition(planeMarker.matrix);
-      model.visible = true
-      scene.add(model);
-    }
-  }
+    // Initialize a planeMarker
+    const planeMarker = createPlaneMarker();
+    scene.add(planeMarker);
 
-  )
-  scene.add(controller);
+    // Create controller
+    const controller = renderer.xr.getController(0);
+    controller.addEventListener('select', () => {
+        if (planeMarker.visible) {
+            const model = deskModel.clone();
+            model.position.setFromMatrixPosition(planeMarker.matrix);
+            model.visible = true;
+            scene.add(model);
+        }
+    });
+    scene.add(controller);
 
-  const camera = new PerspectiveCamera(
-    50,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    100,
-  )
+    const ambientLight = new AmbientLight(0xffffff);
+    scene.add(ambientLight)
 
+    const renderLoop = (timestamp: number, frame?: XRFrame) => {
+        if (renderer.xr.isPresenting) {
+            if (frame) {
+                handleXRHitTest(
+                    renderer,
+                    frame,
+                    (hitPoseTransformed: Float32Array) => {
+                        if (hitPoseTransformed) {
+                            planeMarker.visible = true;
+                            planeMarker.matrix.fromArray(hitPoseTransformed);
+                        }
+                    },
+                    () => {
+                        planeMarker.visible = false;
+                    }
+                );
+            }
+            renderer.render(scene, camera);
+        }
+    };
 
-  const planeMarker = createPlaneMarker();
-
-  scene.add(planeMarker);
-
-  const renderLoop = (timestamp: number, frame?: XRFrame) => {
-    if (renderer.xr.isPresenting) {
-
-      if (frame) {
-        handleXRHitTest(renderer, frame, (hitPoseTransformed: Float32Array) => {
-          if (hitPoseTransformed) {
-            planeMarker.visible = true;
-            planeMarker.matrix.fromArray(hitPoseTransformed);
-          }
-        }, () => {
-          planeMarker.visible = false;
-        })
-      }
-      renderer.render(scene, camera);
-    }
-  }
-
-  renderer.setAnimationLoop(renderLoop);
+    renderer.setAnimationLoop(renderLoop);
 }
